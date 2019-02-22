757171b7477378f489d4dbb1a187e88b396de521
luizgustavo <gusttavodiias@gmail.com>
2019-02-22 15:41:28 -0300

    Querying sparql on the model

--- ProjetoOwl/src/projetoowl/ProjetoOwl.java
+++ ProjetoOwl/src/projetoowl/ProjetoOwl.java
@@ -12,6 +12,7 @@ import static org.apache.jena.enhanced.BuiltinPersonalities.model;
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -42,10 +43,39 @@ import static org.apache.jena.vocabulary.VOID.properties;
 public class ProjetoOwl {
 
     public static void main(String[] args) {
-        OntModel model = ModelFactory.createOntologyModel();
-        model.read("file:/home/luizgustavo/NetBeansProjects/ProjetoOwl/src/projetoowl/ontologyRdfTest.owl");
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
+        model.read("file:/home/luizgustavo/NetBeansProjects/ProjetoOwl/src/projetoowl/ontologyRdfTest.owl", "RDF/XML");
 
-        showOntologyComponents(model, "Data_transformation");
+        //showOntologyComponents(model, "Data_transformation");
+        queryingDataflow(model, "");
+    }
+
+    private static void queryingDataflow(OntModel model, String termo) {
+        String queryString
+                = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "
+                + "select ?uri "
+                + "where { "
+                + "?uri rdfs:subClassOf <http://www.semanticweb.org/luizgustavo/ontologies/2019/1/untitled-ontology-17#Activity>  "
+                + "} \n ";
+
+        org.apache.jena.query.Query query = QueryFactory.create(queryString);
+
+        System.out.println("----------------------");
+
+        System.out.println("Results");
+
+        System.out.println("----------------------");
+
+        System.out.println("Descendentes diretos e indiretos do modelo");
+
+        System.out.println("-------------------");
+
+        QueryExecution qe = QueryExecutionFactory.create(query, model);
+        ResultSet results = qe.execSelect();
+
+        // Output query results    
+        ResultSetFormatter.out(System.out, results, query);
 
     }
 
@@ -63,12 +93,12 @@ public class ProjetoOwl {
 
                 for (StmtIterator j = thisInstance.listProperties(); j.hasNext();) {
                     Statement s = j.next();
-                    System.out.print("    " + s.getPredicate().getLocalName() + " -> ");
+                    System.out.print("      Found property 1: " + s.getPredicate().getLocalName() + " -> ");
 
                     if (s.getObject().isLiteral()) {
-                        System.out.println(s.getLiteral().getLexicalForm());
+                        System.out.println("IF: " + s.getLiteral().getLexicalForm());
                     } else {
-                        System.out.println(s.getObject());
+                        System.out.println("Else: " + s.getObject()); //
                     }
                 }
 
