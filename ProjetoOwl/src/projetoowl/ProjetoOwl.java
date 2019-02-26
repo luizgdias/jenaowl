83d1abfcf1ec87e79f7b53dbabe5ffa5451aee3f
luizgustavo <gusttavodiias@gmail.com>
2019-02-26 12:44:13 -0300

    List all data transformations and storing the results into a java list

--- ProjetoOwl/src/projetoowl/ProjetoOwl.java
+++ ProjetoOwl/src/projetoowl/ProjetoOwl.java
@@ -5,6 +5,7 @@
  */
 package projetoowl;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Properties;
 import javax.management.Query;
@@ -17,6 +18,7 @@ import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.InfModel;
@@ -25,6 +27,7 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.NsIterator;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.RSIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
@@ -46,24 +49,32 @@ public class ProjetoOwl {
         OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_MICRO_RULE_INF);
         model.read("file:/home/luizgustavo/NetBeansProjects/ProjetoOwl/src/projetoowl/ontologyRdfTest.owl", "RDF/XML");
 
+        String termo = "<http://www.semanticweb.org/luizgustavo/ontologies/2019/1/jena#Data_transformation>";
         //showOntologyComponents(model, "Data_transformation");
-        queryingDataflow(model, "");
+        queryingDataflow(model, termo);
     }
 
     private static void queryingDataflow(OntModel model, String termo) {
         String queryString
                 = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
                 + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "
+                + "PREFIX jena: <http://www.semanticweb.org/luizgustavo/ontologies/2019/1/jena#>  "
+                
                 + "select ?uri "
                 + "where { "
-                + "?uri rdfs:subClassOf <http://www.semanticweb.org/luizgustavo/ontologies/2019/1/untitled-ontology-17#Activity>  "
+                + "?uri rdf:type   "+termo
+               
                 + "} \n ";
 
         org.apache.jena.query.Query query = QueryFactory.create(queryString);
+        
+        System.out.println("----------------------");
+
+        System.out.println("Termo utilizado: "+termo);
 
         System.out.println("----------------------");
 
-        System.out.println("Results");
+        System.out.println("Resultado");
 
         System.out.println("----------------------");
 
@@ -74,8 +85,24 @@ public class ProjetoOwl {
         QueryExecution qe = QueryExecutionFactory.create(query, model);
         ResultSet results = qe.execSelect();
 
-        // Output query results    
-        ResultSetFormatter.out(System.out, results, query);
+        // Output query results   
+        //showing off the query results about all Data_transformations
+        //ResultSetFormatter.out(System.out, results, query);
+        
+        ArrayList<String> arlist = new ArrayList<>();
+        
+        while (results.hasNext()) {
+           arlist.add(results.next().toString());
+        }
+        
+        //System.out.println(arlist.size());
+        
+        for (int i = 0; i < arlist.size(); i++) {
+            System.out.println(arlist.get(i));
+            
+        }
+        
+        
 
     }
 
